b301acce20fb035c105c7a6e4d4cfd4144eac312
Mohit Deshpande <mohitdeshpande@icloud.com>
2018-02-12 13:06:50 -0500

    Implement login method
    Implemented login method which returns a JWT token if the username and password is valid

---
+++ MomentuumApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using MomentuumApi.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MomentuumApi.Controllers
+{
+    [Route("api/auth/[action]")]
+    public class AuthenticationController : Controller
+    {
+
+        private IConfiguration _config;
+        private readonly MobileDBContext _context;
+
+        public AuthenticationController(IConfiguration config, MobileDBContext context) 
+        {
+            _config = config;
+            _context = context;
+        }
+
+        // POST api/auth/login
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody]LoginModel login)
+        {
+            IActionResult response = Unauthorized();
+
+            TblEmployees employee = _context.TblEmployees.FirstOrDefault(emp => emp.EmployeeLogin.Equals(login.Username));
+
+            if (employee != null && employee.UsrPassword.Equals(login.Password)) {
+                var tokenString = BuildToken();
+
+                response = Ok(new { token = tokenString });
+            }
+
+            return response;
+        }
+
+        // POST api/auth/logout
+        [HttpPost]
+        public IActionResult Logout([FromBody]string value)
+        {
+            IActionResult response = BadRequest(new {error = "Token is Invalid"});
+
+            return response;
+        }
+
+        /// <summary>
+        /// Builds the JwtToken for the user
+        /// </summary>
+        /// <returns>The token.</returns>
+        /// <param name="user">User.</param>
+        private string BuildToken()
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+                                             _config["Jwt:Issuer"],
+                                             expires: DateTime.Now.AddDays(Double.Parse(_config["Jwt:ExpireDays"])),
+                                             signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ MomentuumApi/Model/LoginModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace MomentuumApi.Model
+{
+    public class LoginModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
