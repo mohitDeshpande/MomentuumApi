b310b9058505057233e02224fd9f390157314bd1
Soaib Momin <smomin20@gmail.com>
2018-02-21 10:26:24 -0500

    Changed EmployeeLogin id to Jwt Token to fetch the case list

--- MomentuumApi/Controllers/CaseController.cs
+++ MomentuumApi/Controllers/CaseController.cs
@@ -7,26 +7,27 @@ using Microsoft.AspNetCore.Mvc;
 using MomentuumApi.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authorization;
+using MomentuumApi.Utils;
 
 namespace MomentuumApi.Controllers
 {
-	[Produces("application/json")]
-	[Route("api/[controller]")]
+    [Produces("application/json")]
+    [Route("api/[controller]")]
     public class CaseController : Controller
     {
-		private readonly MobileDBContext _context;
+        private readonly MobileDBContext _context;
 
-		public CaseController(MobileDBContext context)
-		{
-			_context = context;
-		}
+        public CaseController(MobileDBContext context)
+        {
+            _context = context;
+        }
 
-		// GET: api/case
-		[HttpGet, Authorize]
-		public IEnumerable<TblCase> GetAll()
-		{
-			return _context.TblCase.ToList();
-		}
+        // GET: api/case
+        [HttpGet, Authorize]
+        public IEnumerable<TblCase> GetAll()
+        {
+            return _context.TblCase.ToList();
+        }
 
 
         // GET api/case/{id}
@@ -46,24 +47,44 @@ namespace MomentuumApi.Controllers
             return _context.TblCase.Where(emp => emp.CaseAssignedTo.Equals(id)).ToList();
         }
 
-        
 
-        // GET: api/case/client/emp/{id}
+
+        // GET: api/case/client/emp
         // getting all the cases with client details assigned to the Employee based on employeelogin
-        [HttpGet("client/emp/{id}"), Authorize]
-		public IActionResult GetCaseClientByEmpId(string id)
-		{
+        [HttpGet("client/emp"), Authorize]
+        public IActionResult GetCaseClientByEmpJwt(string id)
+        {
+            var user = JwtHelper.GetUser(HttpContext.User.Claims);
 
             var clientCase = _context.TblCase
                 .Join(_context.TblClient, cas => cas.IdClient, cli => cli.Id, (cas, cli) => new { cas, cli })
-                .Where(x=> x.cas.CaseAssignedTo == id)
+                .Where(x => x.cas.CaseAssignedTo == id)
                 .ToList();
 
-			if (clientCase == null)
-			{
-				return NotFound();
-			}
-			return new ObjectResult(clientCase);
-		}
-	}
+            if (clientCase == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(clientCase);
+        }
+
+
+        // GET: api/case/client/{id}
+        // getting all the cases with client details by case id
+        [HttpGet("client/{id}"), Authorize]
+        public IActionResult GetCaseClientById(int id)
+        {
+     
+            var clientCase = _context.TblCase
+                .Join(_context.TblClient, cas => cas.IdClient, cli => cli.Id, (cas, cli) => new { cas, cli })
+                .Where(x => x.cas.Caseid == id)
+                .ToList();
+
+            if (clientCase == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(clientCase);
+        }
+    }
 }
