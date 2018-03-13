4ccc2adf57b7f41c1ee5ace3b92c056d094cd005
Mohit Deshpande <mohitdeshpande@icloud.com>
2018-03-12 22:05:14 -0400

    employee Controller to return employee details

---
+++ MomentuumApi/Controllers/EmployeeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MomentuumApi.Model.CivicTrack;
+using MomentuumApi.Utils;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MomentuumApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class EmployeeController : Controller
+    {
+        private readonly CivicTrackContext _context;
+
+        public EmployeeController(CivicTrackContext context) 
+        {
+            _context = context;
+        }
+
+        // GET: api/employee
+        [HttpGet, Authorize]
+        public IActionResult Get()
+        {
+            var employeeId = JwtHelper.GetUser(HttpContext.User.Claims);
+            try
+            {
+                TblEmployees employee = _context.TblEmployees
+                                                .Where(emp => emp.EmployeeLogin.Equals(employeeId))
+                                                .Single();
+                employee.UsrPassword = null;
+                return new ObjectResult(employee);
+            } catch (InvalidOperationException e) {
+                Console.WriteLine(e);
+                return NotFound();
+            }
+        }
+
+    }
+}
