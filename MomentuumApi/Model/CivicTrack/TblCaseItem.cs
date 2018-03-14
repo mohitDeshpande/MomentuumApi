3443e4232d77f4e83c447bf874b2f1ebf2cb096b
DivineBrad <bradleymjblanchard@gmail.com>
2018-03-14 05:05:14 -0400

    Revert "Revert "Added, mappings and entities for Case and Case Item tables""
    This reverts commit 5ab3d91e6783ddae52350899b1cc7ee6d16cd0d2.

---
+++ MomentuumApi/Model/CivicTrack/TblCaseItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MomentuumApi.Model.CivicTrack
+{
+    public partial class TblCaseItem
+    {
+        public int? IntId { get; set; }
+        public int Caseid { get; set; }
+        public String CaseItemDate { get; set; }
+        public String CaseItemDescription { get; set; }
+        public String CaseItemDetail { get; set; }
+        public String CaseItemStatus { get; set; }
+        public String CaseItemAction { get; set; }
+        public String CaseItemAssigned { get; set; }
+        public String CaseItemFollowUpdate { get; set; }
+        public String Deleted { get; set; }
+        public String UserId { get; set; }
+        public String TimeProcess { get; set; }
+        /*not in new db
+        public String Priority { get; set; }
+        public String CreatedBy { get; set; }
+        public String CreatedDate { get; set; }
+        public String UpdatedDate { get; set; }
+        */
+    }
+}
