e178649e260f88c3155514a672432c91cf3b4917
Soaib Momin <smomin20@gmail.com>
2018-03-27 10:27:01 -0400

    Add Case, Create Case ITem and Case Types

---
+++ MomentuumApi/Model/CivicTrack/TblCaseType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MomentuumApi.Model.CivicTrack
+{
+    public class TblCaseType
+    {
+        public int id { get; set; }
+        public String listtext { get; set; }
+        public String Code { get; set; }
+        public String refid { get; set; }
+        public String score { get; set; }
+    }
+}
