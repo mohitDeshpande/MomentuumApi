d48342d2b1fab1ee58710d1da393dd3813f46a93
Soaib Momin <smomin20@gmail.com>
2018-03-27 16:11:58 -0400

    working backend for case-update

--- MomentuumApi/Startup.cs
+++ MomentuumApi/Startup.cs
@@ -30,9 +30,11 @@ namespace MomentuumApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-			//var oldConnection = $"Server={Configuration["database:server"]},{Configuration["database:port"]};Database={Configuration["database:db"]};Trusted_Connection=False;uid={Configuration["database:uid"]};pwd={Configuration["database:pwd"]}";
-			//services.AddDbContext<MobileDBContext>(options => options.UseSqlServer(oldConnection));
-			var civicTrackConnection = $"Server={Configuration["database:server"]},{Configuration["database:port"]};Database={Configuration["database:civicTrackDb"]};Trusted_Connection=False;uid={Configuration["database:uid"]};pwd={Configuration["database:pwd"]}";
+             var oldConnection = $"Server={Configuration["database:server"]},{Configuration["database:port"]};Database={Configuration["database:db"]};Trusted_Connection=False;uid={Configuration["database:uid"]};pwd={Configuration["database:pwd"]}";
+             services.AddDbContext<MobileDBContext>(options => options.UseSqlServer(oldConnection));
+
+            var civicTrackConnection = @"Server=.\SQLEXPRESS;Database=VT;Trusted_Connection=True;";
+            //var civicTrackConnection = $"Server={Configuration["database:server"]},{Configuration["database:port"]};Database={Configuration["database:civicTrackDb"]};Trusted_Connection=False;uid={Configuration["database:uid"]};pwd={Configuration["database:pwd"]}";
              services.AddDbContext<CivicTrackContext>(options => options.UseSqlServer(civicTrackConnection));
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
